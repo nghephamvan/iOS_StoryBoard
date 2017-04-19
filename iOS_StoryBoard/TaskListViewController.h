9bae85e94a7f0adcef63321948ceb8b48ed421f5
Nghe Pham <ilovetayninh@gmail.com>
2017-04-19 10:30:30 +0700

    [Update]Add model and view

---
+++ iOS_StoryBoard/TaskListViewController.h
@@ -0,0 +1,15 @@
+//
+//  TaskListViewController.h
+//  iOS_StoryBoard
+//
+//  Created by TMA103 on 4/19/17.
+//  Copyright © 2017 TMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TaskListViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *tasks;
+
+@end
